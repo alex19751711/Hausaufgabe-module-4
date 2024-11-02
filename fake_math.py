86581bd85c7e120b8b286a8511d739132cd566ac
alex19751711 <avgrishin1975@gmail.com>
2024-11-02 20:37:06 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide (first, second):
+    if second == 0:
+        error_message = 'Ошибка'
+        return error_message
+    else:
+        div_result = first / second
+        return div_result
